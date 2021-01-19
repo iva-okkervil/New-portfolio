11a6d8e6f39541d0ca9de34c896a49dad7d80242
Anastasia <cheshire_tomcat@list.ru>
2021-01-19 21:51:49 +0300

    mobilefirst commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,21 +1,33 @@
-let containerE1 = document.querySelector('#portfolio__works');
-let mixit = mixitup(containerE1, {
-	classNames: {
-	}
-});
+$(document).ready(function () {
+  let containerE1 = document.querySelector('#portfolio__works');
+  let mixit = mixitup(containerE1, {
+    classNames: {
+    }
+  });
 
-$(function() {
-  function init() {
-    $('[data-behaviour="toggle-menu-icon"]').on('click', toggleMenuIcon);
-    $('[data-behaviour="toggle-link-icon"]').on('click', toggleSubMenu);
-  };
-  
-  function toggleMenuIcon() {
-    $(this).toggleClass('active');
-    $('[data-element="toggle-nav"]').toggleClass('active');
-    $('[data-element="overlay"]').toggleClass('active');
-    $('[data-element="no-scroll"]').toggleClass('active');
-  };
-  
-  init()
+  $(function() {
+    function init() {
+      $('[data-behaviour="toggle-menu-icon"]').on('click', toggleMenuIcon);
+      $('[data-behaviour="toggle-link-icon"]').on('click', toggleSubMenu);
+    };
+    
+    function toggleMenuIcon() {
+      $(this).toggleClass('active');
+      $('[data-element="toggle-nav"]').toggleClass('active');
+      $('[data-element="overlay"]').toggleClass('active');
+      $('[data-element="no-scroll"]').toggleClass('active');
+    };
+    
+    init()
+  });
+
+  $('#backtop').hide('');
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 500){
+      $('#backtop').fadeIn();
+    }
+    else{
+      $('#backtop').fadeOut();
+    }
+  });
 });
--- docs/projects/portfolio/docs/js/main.js
+++ docs/projects/portfolio/docs/js/main.js
@@ -1,87 +1,9 @@
-let containerE1 = document.querySelector('#portfolio__works');
+$(document).ready(function () {
+  let containerE1 = document.querySelector('#portfolio__works');
 let mixit = mixitup(containerE1, {
-	classNames: {
-	}
+  classNames: {
+  }
 });
-
-$(document).ready(function(){
-	// form placeholder
-	const formItems = document.querySelectorAll('.contacts__form-input');
-	
-	for(let item of formItems){
-		const thisParent = item.closest('.contacts__form-item');
-		const thisPlaceholder = thisParent.querySelector('.fake-placeholder');
-		// Если инпут в фокусе		
-		item.addEventListener('focus', function(){
-			thisPlaceholder.classList.add('active');
-		});
-
-		// Если инпут теряет фокус
-		item.addEventListener('blur', function(){
-
-			if(item.value.length > 0){
-				thisPlaceholder.classList.add('active');
-			}
-			else{
-				thisPlaceholder.classList.remove('active');
-			}
-		})
-	}
-});
-
-//FORM VALIDATE
-	$('.contacts__form').validate({
-		rules: {
-			email: {
-				required: true,
-				email: true
-			},
-			subject: {
-				required: true
-			},
-			comment: {
-				required: true
-			}
-		},
-		messages: {
-			email: {
-				required: 'Введите e-mail',
-				email: 'отсутсвует символ @'
-			},
-			subject: {
-				required: 'Введите тему сообщения'
-			},
-			comment: {
-				required: 'Введите текст сообщения'
-			}
-		},
-		submitHandler: function (form) {
-			ajaxFormSubmit();
-		}
-
-	})
-
-	// Функция AJAX запроса на сервер
-
-	function ajaxFormSubmit() {
-
-		let string = $(".contacts__form").serialize();
-
-		//Формируем ajax-запрос
-		$.ajax({
-			type: "POST",
-			url: "php/mail.php", 
-			data: string, 
-			// Функция, если все прошло успешно
-			success: function (html) {
-				$(".contacts__form").slideUp(800);
-				$('#answer').html(html);
-			}
-		});
-		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
-		return false;	
-}
-
 
 $(function() {
   function init() {
@@ -98,3 +20,15 @@ $(function() {
   
   init()
 });
+
+$('#backtop').hide('');
+$(window).scroll(function(){
+  if($(this).scrollTop() > 500){
+    $('#backtop').fadeIn();
+  }
+  else{
+    $('#backtop').fadeOut();
+  }
+});
+});
+
